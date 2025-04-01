5fd9b82e9b038ffd9d8858100e360875c27bd7b6
jungwoo0601 <pjw1346799@naver.com>
2025-04-01 11:02:08 +0900

    JS (입출력~if문)

---
+++ workspace/ch01/ex01-05.js
@@ -0,0 +1,32 @@
+/*
+변수 선언
+*/
+
+var message1 = "아자핑이 '안녕하세요' 라고 말했다"; 
+var message2 = '아자핑이 "안녕하세요"라고 말했다'; 
+var message3 = '아자핑이 \'안녕하세요\' 라고 말했다 '; 
+var message4 = "몰라핑이 \n모른다고 말\t했다"; 
+
+var $money = 4;
+var $4 = 4;
+
+var pi = 3.14;
+
+var_=200;
+
+var userName = '낙타핑';
+var userAge = 30; var userGender = "여자";
+var userEmail= 'ping@gmail.com';
+
+
+console.log('ping@gmail.com');
+console.log(userEmail);
+
+userEmail='ping@naver.com';
+console.log(userEmail);
+
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
+console.log(message4);
---
+++ workspace/ch01/ex01-16-02.js
@@ -0,0 +1,15 @@
+/*
+if문을 사용하여 학점을 출력하세요.
+*/
+
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score < 90) {
+  console.log("B");
+} else if (score >= 70 && score < 80) {
+  console.log("C");
+} else if (score >= 60 && score < 70) {
+  console.log("D");
+} else if (score < 60) {
+  console.log("F");
+}
