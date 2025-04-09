e4e4012e54be1ac00ee19e2d9b6b905879c5f3a5
jungwoo0601 <pjw1346799@naver.com>
2025-04-09 16:37:56 +0900

    배열열

--- workspace/ch01/ex01-27.js
+++ workspace/ch01/ex01-27.js
@@ -2,5 +2,16 @@
 배열의 모든 요소 읽기 1 - for 루프, for...of, for...in
 */
 
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+//배열의 모든 요소 출력(for)
+console.log("for");
+for (let i = 0; i < arr.length; i++) {
+  const elem = arr[i];
+  console.log(i, elem);
+}
 
+// 배열 모든 요소의 합계, 홀수값의 합계, 짝수값의 합계를 출력
+const sum = 0;
+
+console.log("합계", sum);
