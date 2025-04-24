f91b05fae37af43fe6089da0d76a7c1bcabde7ff
jungwoo0601 <pjw1346799@naver.com>
2025-04-24 14:40:27 +0900

    [docs] prototype

--- workspace/ch03/ex03-04.js
+++ workspace/ch03/ex03-04.js
@@ -1,15 +1,13 @@
 /*
-* 프로토타입 체인을 이용한 상속 기능 구현
-*/
+ * 프로토타입 체인을 이용한 상속 기능 구현
+ */
 
 /**
  * 고등학교 성적관리 생성자 함수(총점과 평균 계산)
  * @param {number} kor 국어 점수
  * @param {number} eng 영어 점수
  */
-function HighSchool(kor, eng){
-
-}
+function HighSchool(kor, eng) {}
 
 const s1 = new HighSchool(100, 91);
 console.log(s1.sum());
@@ -20,11 +18,27 @@ console.log(s1.avg());
  * @param {number} kor 국어 점수
  * @param {number} eng 영어 점수
  */
-function College(kor, eng){
-
+function College(kor, eng) {
+  this.kor = kor;
+  this.eng = eng;
 }
 
-const c1 = new College(80, 71);
+College.prototype = new HighSchool();
+College.prototype.constructor = College;
+College.prototype.sum = function () {
+  return this.kor + this.eng;
+};
+
+College.prototype.grade = function () {
+  const avg = this.avg();
+  if (avg >= 90) return "A";
+  else if (avg >= 80) return "B";
+  else if (avg >= 70) return "C";
+  else if (avg >= 60) return "D";
+  else return "F";
+};
+
+const c1 = new College(80, 99);
 console.log(c1.sum());
 console.log(c1.avg());
 console.log(c1.grade());
