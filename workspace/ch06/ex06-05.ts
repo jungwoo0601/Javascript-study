9a675b6f613aa3bb5eb3516df4efd8feed196b9d
jungwoo0601 <pjw1346799@naver.com>
2025-04-28 14:14:44 +0900

    ex06 interface (typescript)

--- workspace/ch06/ex06-05.ts
+++ workspace/ch06/ex06-05.ts
@@ -1,6 +1,6 @@
 // 유니언 타입(union type)
 
-(()=>{
+(() => {
   function logString(msg: string) {
     console.log(msg);
   }
@@ -8,6 +8,15 @@
     console.log(msg);
   }
 
+  logString("hello");
+  logNumber(123);
 
+  //유니언 타입
+  // | 연산자로 여러 타입을 지정
+  function log(msg: string | number) {
+    console.log(msg);
+  }
 
+  log("hello");
+  log(123);
 })();
