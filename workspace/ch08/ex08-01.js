4f93170c64e2660a1ba253a86788075b3db68871
jungwoo0601 <pjw1346799@naver.com>
2025-05-07 10:32:53 +0900

    [docs] ex08-01,02 ts

---
+++ workspace/ch08/ex08-01.js
@@ -0,0 +1,14 @@
+"use strict";
+// 에러 핸들링 - Error 객체
+(() => {
+    function f1() {
+        const err = new Error("에러 발생");
+        console.log(err.name); // Error
+        console.log(err.message); // 에러 발생
+        console.log(err.stack); // 콜 스택 정보 출력
+    }
+    function f2() {
+        f1();
+    }
+    f2();
+})();
---
+++ workspace/ch08/ex08-02.js
@@ -0,0 +1,16 @@
+"use strict";
+// 에러 핸들링 - 에러가 발생하는 경우
+(() => {
+    function f1() {
+        // const obj = null as any;
+        const obj = undefined;
+        //obj.fn(); //TypeError 발생
+        const fn = new Function("x", "y", "return x + y");
+        console.log(fn(10, 20));
+    }
+    function f2() {
+        f1();
+    }
+    f2();
+    console.log("프로그램 종료");
+})();
